207d67fc845537f31d83f5d9abb9033f535357f7
ashisa <ashisa78@live.com>
2020-03-23 13:34:48 +0530

    Update azurecli-script.sh

--- script-cmd/azurecli-script.sh
+++ script-cmd/azurecli-script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-if [ "$1" == "" ] || [ "$2" == "" ]
+ARG=$1
+RG=$(echo $ARG |awk '{print $1}')
+VM=$(echo $ARG |awk '{print $2}')
+if [ "$RG" == "" ] || [ "$VM" == "" ]
 then
     echo "syntax: $0 resourcegroup-name vm-name"
     echo 
     echo "Send the argument using this URL - http://127.0.0.1:8080/arg=myrg%20myvm01"
 else
     az login
-    az group create --name $1 --location southindia
-    az vm create --name $2 --resource-group $1 --image UbuntuLTS --admin-username 'vmadmin' --admin-password 'R$ND0MPA%%WD'
+    az group create --name $RG --location southindia
+    az vm create --name $VM --resource-group $RG --image UbuntuLTS --admin-username 'vmadmin' --admin-password 'R$ND0MPA%%WD'
 fi
